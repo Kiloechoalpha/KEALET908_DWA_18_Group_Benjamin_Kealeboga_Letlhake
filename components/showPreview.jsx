76408785393e374546e34fa70834ee32e26d1850
Kealeboga Letlhake <125870519+Kiloechoalpha@users.noreply.github.com>
2024-02-16 10:12:06 +0200

    Update showPreview.jsx
    Replace "Loading..." with a spinner to achieve a better loading state

--- components/showPreview.jsx
+++ components/showPreview.jsx
@@ -1,19 +1,12 @@
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useParams, Link } from 'react-router-dom';
+import Spinner from './Spinner'; 
 
-/**
- * ShowPreview component displays details of a podcast show.
- * It fetches show details from an API based on the show ID provided in the URL parameters.
- * @returns {JSX.Element} - The rendered JSX element containing show details.
- */
 const ShowPreview = () => {
-  // Extracting show ID from the URL parameters using useParams hook
   const params = useParams();
-  
-  // State to store the retrieved show details
   const [show, setShow] = useState(null);
+  const [loading, setLoading] = useState(true); // State to manage loading state
 
-  // useEffect hook to fetch show details when the component mounts or show ID changes
   useEffect(() => {
     // Fetching show details from the API using the show ID
     fetch(`https://podcast-api.netlify.app/id/${params.id}`)
@@ -21,36 +14,32 @@ const ShowPreview = () => {
       .then((data) => {
         // Updating the state with the retrieved show details
         setShow(data);
+        setLoading(false); // Set loading to false once data is fetched
       })
       .catch((error) => {
         console.error('Error fetching show details:', error);
+        setLoading(false); // Set loading to false in case of an error
       });
-  }, [params.id]); // Dependency array ensures the effect runs when show ID changes
+  }, [params.id]);
 
-  // Conditional rendering when show details are still being fetched
+  // Conditional rendering for loading state
+  if (loading) {
+    return <Spinner />;
+  }
+
   if (!show) {
     return <div>Loading Show Details...</div>;
   }
 
-  // Rendering show details once they are available
   return (
     <div className="container">
       <h2>Show Details</h2>
-      
-      {/* Card displaying show details */}
       <div className="card" style={{ width: '18rem' }}>
         <img src={show.image} alt={show.title} className="card-img-top" />
-        
         <div className="card-body">
-          {/* Show title */}
           <h5 className="card-title">{show.title}</h5>
-          
-          {/* Show description */}
           <p className="card-text">{show.description}</p>
-          
         </div>
-
-        {/* Link to navigate to the Seasons page for the current show */}
         <Link to={`/showPreview/${show.id}`} className="btn btn-outline-secondary rounded-pill">
           Seasons
         </Link>
@@ -59,5 +48,4 @@ const ShowPreview = () => {
   );
 };
 
-// Exporting the ShowPreview component as the default export
 export default ShowPreview;
